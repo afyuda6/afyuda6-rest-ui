cc996dd4f2552bd8687b24838af12d0c6cde8a5f
Yuda <yuda_p411@apps.ipb.ac.id>
2024-12-26 17:19:16 +0700

    26/12/2024

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let apiUrl = 'https://rest-api-cpp-640830702257.asia-southeast1.run.app/users';
+let apiUrl = 'https://rest-api-cpp-6ujvrm2fma-as.a.run.app/users';
 const usersTable = document.getElementById('users-list');
 const createForm = document.getElementById('create-form');
 const nameInput = document.getElementById('name');
@@ -111,7 +111,7 @@ async function updateUser(id, name) {
 }
 
 async function deleteUser(id) {
-    const specificUrl = "https://rest-api-go-640830702257.asia-southeast1.run.app/users";
+    const specificUrl = "https://rest-api-go-6ujvrm2fma-as.a.run.app/users";
     const isSpecificUrl = apiUrl === specificUrl;
 
     let response;
